8c8d11918cce542dce3f48fdd40eb854ceb36048
voyedapo632 <voyedapo632@lsr7.net>
2025-08-28 12:41:42 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,9 +1,34 @@
 public class Example {
-  public static void main(String[] args) {
-    
-  }
 
-  private float toRad(float deg) {
-    return deg / (180 / Math.PI);
-  }
+    private final static double PI = 3.141;
+
+    public static void main(String[] args) {
+        // Declear floating point number called "myNumber"
+        double myNumber;
+
+        /**
+         * Assignment statement
+         * Assigns 45 to "myNumber"
+        */
+        myNumber = 45.0;
+
+        // Print number in degrees
+        System.out.println("Degrees: " + myNumber);
+
+        /**
+         * Assignment Statement Convert number from degrees to radians
+        */
+        myNumber = toRad(myNumber);
+
+        // Print number in degrees
+        System.out.println("Radians: " + myNumber);
+    }
+
+    /**
+     * @Test Converts degrees to radians.
+     * @param deg type double refers to degrees
+     */
+    private static double toRad(double deg) {
+        return deg * PI / 180.0;
+    }
 }
